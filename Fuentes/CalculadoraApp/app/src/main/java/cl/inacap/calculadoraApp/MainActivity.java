e2da079c709776abcfb1939add9dda5adbbb077a
Camila González G <camilag@gmail.com>
2020-09-06 15:41:35 -0300

    Generar "Hola Mundo en Calculadora Generar estructura en proyecto android Generar paleta de colores Generar layout base

---
+++ Fuentes/CalculadoraApp/app/src/main/java/cl/inacap/calculadoraApp/MainActivity.java
@@ -0,0 +1,21 @@
+package cl.inacap.calculadoraApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void saludar(View v){
+        Toast.makeText(this, "Boton de prueba", Toast.LENGTH_LONG).show();
+    }
+
+}
